d4e6f3582f03aa135a9e7d85c099d84b74aa5927
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-05-30 21:01:38 -0400

    Version 1 - integers and double variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    int intOperandA = 1;
+    int intOperandB = 5;
+    int intSum = 12;
+    int intProduct = 30;
+    int intDifference = 11;
+    int intQuotient = 36;
+    int intModulo = 44;
+    double doubleOperandA = 1.5;
+    double doubleOperandB = 2.6;
+    double doubleSum = 12.2;
+    double doubleProduct = 24.5;
+    double doubleDifference = 9.1;
+    double doubleQuotient = 4.5;
+
+
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
